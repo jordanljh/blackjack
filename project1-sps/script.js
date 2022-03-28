6e74d4653fe49300e85cd1946eb58cfb0d27909a
jordanlow123 <101250926+jordanlow123@users.noreply.github.com>
2022-03-29 03:46:57 +0800

    Update on 29/3

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,55 @@
+var roll3Number = function () {
+  // generate decimal 0 <= x < 3
+  var randomDecimal = Math.random() * 3;
+
+  // generate integer 0 <= x <= 2
+  var randomInteger = Math.floor(randomDecimal);
+
+  var chosenNumber = randomInteger + 1;
+  return chosenNumber;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var key = roll3Number();
+  var computer;
+
+  // computer choice
+  if (key == 1) {
+    computer = "scissors";
+  } else if (key == 2) {
+    computer = "paper";
+  } else {
+    computer = "stone";
+  }
+  var myOutputValue = `Player: ${input}, Computer: ${computer}, Result: `;
+
+  // check results
+  if (input == "scissors") {
+    if (computer == "scissors") {
+      myOutputValue += "Draw.";
+    } else if (computer == "paper") {
+      myOutputValue += "Win.";
+    } else {
+      myOutputValue += "Lose.";
+    }
+  } else if (input == "paper") {
+    if (computer == "paper") {
+      myOutputValue += "Draw.";
+    } else if (computer == "stone") {
+      myOutputValue += "Win.";
+    } else {
+      myOutputValue += "Lose.";
+    }
+  } else if (input == "stone") {
+    if (computer == "stone") {
+      myOutputValue += "Draw.";
+    } else if (computer == "scissors") {
+      myOutputValue += "Win.";
+    } else {
+      myOutputValue += "Lose.";
+    }
+  } else {
+    return `Please choose between scissors, paper and stone.`;
+  }
   return myOutputValue;
 };
