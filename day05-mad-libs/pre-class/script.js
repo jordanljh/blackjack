a2fc9263f75863ef774d872fccb81d61b43f2c91
Jin Hui <101250926+jordanlow123@users.noreply.github.com>
2022-04-04 13:56:38 +0800

    Updated on 4/4 1400

--- day05-mad-libs/pre-class/script.js
+++ day05-mad-libs/pre-class/script.js
@@ -1,11 +1,48 @@
+// Store guesses in a global array that persists across Submits.
+var guesses = [];
+
+var rollDice = function () {
+  let num = Math.random() * 6;
+  num = Math.ceil(num);
+  return num;
+};
+
 var followArraysMain = function (input) {
   // Attempt the Follow Along exercise from the Arrays module below with followArraysMain as the main function.
-  var myOutputValue = 'hello world';
+  // Add the user's guess to the guesses array.
+  guesses.push(input);
+
+  // Generate a random dice number.
+  var randomDiceNumber = rollDice();
+
+  // Initialise output to communicate loss.
+  // Output the record of all guesses regardless of loss or win.
+  var myOutputValue = "You lose. Your guesses: " + guesses;
+
+  // If the guess matches the dice roll, change output to communicate win.
+  if (randomDiceNumber == input) {
+    myOutputValue = "You win. Your guesses: " + guesses;
+  }
+  // Return output value.
   return myOutputValue;
 };
 
+let names = [];
+
 var followArraysLoopsMain = function (input) {
   // Attempt the Follow Along exercise from the Looping Over An Array module below with followArraysLoopsMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  let found = false;
+  for (let index = 0; index < names.length; index += 1) {
+    let currentName = names[index];
+    if (currentName == input) {
+      found = true;
+    }
+  }
+
+  if (!found) {
+    names.push(input);
+  }
+
+  let message = "Names: " + names;
+  return message;
 };
